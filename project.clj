3872f9d3e99a94d8f0ee4734b2f231bdf024b95b
Oliver McCormack <oli@climate.com>
2014-11-25 10:31:17 -0800

    Increase scala version to 2.10.4
    Confirmed that the test project runs correctly with this change.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-scalac "0.1.0"
+(defproject lein-scalac "0.1.1"
   :eval-in-leiningen true
-  :dependencies [[org.scala-lang/scala-compiler "2.9.1"]
+  :dependencies [[org.scala-lang/scala-compiler "2.10.4"]
                  [lancet "1.0.1"]])
--- test-project/project.clj
+++ test-project/project.clj
@@ -1,9 +1,9 @@
 (defproject test-project "0.1.0"
   :description "test project with some scala"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.scala-lang/scala-library "2.9.1"]]
+                 [org.scala-lang/scala-library "2.10.4"]]
   :scala-source-path "scala"
-  :plugins [[lein-scalac "0.1.0"]]
+  :plugins [[lein-scalac "0.1.1"]]
   :prep-tasks ["scalac"]
   ;; for Leiningen 1.x you need this instead:
   ;; :hooks [leiningen.hooks.scalac]
